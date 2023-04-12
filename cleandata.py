1b8e09235825423da6642e6672516e2d026e5990
joshshipton <shiptonjosh@gmail.com>
2023-04-12 11:33:49 +0800

    added sqlite and training data but need to fixdata

--- cleandata.py
+++ cleandata.py
@@ -2,9 +2,14 @@ import pandas as pd
 import sqlite3
 import os
 import json
-path = 'C:\\Users\\Shipt\\Desktop\\chatbot\\data\\messages\\inbox'
+import datetime
+folder_path = 'C:\\Users\\Shipt\\Desktop\\chatbot\\data\\messages\\inbox'
+db = sqlite3.connect('database.db')
+cursor = db.cursor()
+data = {'text': [], 'handle_id': [], 'is_from_me': [], 'date': []}
 
-for root, dirs, files in os.walk(path):
+
+for root, dirs, files in os.walk(folder_path):
     for dir in dirs:
         # Get a list of all the JSON files in the subdirectory
         json_files = [f for f in os.listdir(
@@ -12,23 +17,68 @@ for root, dirs, files in os.walk(path):
 
         # Loop through the JSON files
         for json_file in json_files:
-            with open(os.path.join(root, dir, json_file), 'r') as f:
+            with open(os.path.join(root, dir, json_file), 'r', encoding='utf-8') as f:
                 json_data = json.load(f)
 
             # Extract the required data from the JSON object
-            participants = json_data['participants']
             messages = json_data['messages']
 
             # Do something with the data
-            for participant in participants:
-                print(participant['name'])
-            
             for message in messages:
                 try:
-                    print(message['sender_name'])
-                    print(message['content'])
-                except(KeyError):
+                    data['text'].append(message['content'])
+                    # uses binary to flag if was sent by me or not
+                    who_from = (message['sender_name'])
+                    if who_from == 'Josh Shipton' or who_from == 'joshshipo':
+                        data['is_from_me'].append(1)
+                    else:
+                        data['is_from_me'].append(0)
+
+                    data['handle_id'].append(who_from)
+
+                    # converts the weird timestamp fb gives into normal data
+                    timestamp_ms = message['timestamp_ms']
+                    date_time = datetime.datetime.fromtimestamp(
+                        timestamp_ms/1000.0)
+                    data['date'].append(date_time)
+
+                except (KeyError):
                     continue
 
 
+df = pd.DataFrame(data)
 
+
+df.dropna(subset=['text'], inplace=True)
+
+
+
+def make_sentences(series):
+    return '. '.join(series)
+
+
+train_data = pd.DataFrame(columns=['text', 'response'])
+
+# iterate thru each convo
+for person in pd.unique(df['handle_id']):
+    conversation = df[df['handle_id'] == person]
+    grouped = (conversation.groupby(conversation.is_from_me.diff().ne(0).cumsum(), as_index=False)
+               .agg({'text': make_sentences, 'is_from_me': 'first',
+                     'handle_id': 'first', 'date': 'first'}))
+    tmp = pd.DataFrame({'text': list(conversation['text'][0:-1]),
+                        'response': list(conversation['text'][1:])})
+
+    train_data = pd.concat(
+        [train_data, tmp[['text', 'response']]], ignore_index=True)
+
+# make lowercase
+train_data['text'] = train_data['text'].apply(lambda x: x.lower())
+train_data['response'] = train_data['response'].apply(lambda x: x.lower())
+
+
+print(train_data.head(50))
+train_data.to_sql('chatbot', db, if_exists='replace', index=False)
+
+# Read the data from the database to confirm that it was written correctly
+df = pd.read_sql_query('SELECT * FROM chatbot', db)
+print(df.head())
